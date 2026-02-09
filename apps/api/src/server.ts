7aa71796fd26543e9502cd9a2a227dc4595d1cf5
Naufal <nazor.dz@gmail.com>
2026-02-09 22:35:50 +0700

    first init for boilerplate

---
+++ apps/api/src/server.ts
@@ -0,0 +1,24 @@
+import dotenv from "dotenv";
+import express, { Express } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+dotenv.config();
+
+const app: Express = express();
+const port = "8000";
+
+app.use(bodyParser.json({ limit: "10mb" }));
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    limit: "50mb",
+  })
+);
+app.use(express.json());
+
+
+app.listen(port, () => {
+  console.log(`server is running at http://localhost:${port}`);
+});
